63d91ff8da64a3724f0056e42007074fc17e8eb3
KARAN THAKUR <karannapit7@gmail.com>
2026-01-08 20:30:44 +0530

    Update 2_practies_string.py

--- 2_practies_string.py
+++ 2_practies_string.py
@@ -1,10 +1,11 @@
-# String Reversal: Write a program to reverse a given string.
+#String Reversal: Write a program to reverse a given string.
 
 my_str = ("Python is the best ")
 reversed_string = my_str[::-1]
 print(reversed_string)
 
 #Length Check: Write a function that returns the length of a string without using the built-in len() function.
+
 my_str1 = ("i am a b-tech student , i am a very strong")
 my_str2 = len(my_str)
 print(my_str1)
@@ -14,3 +15,4 @@ print(my_str1)
 my_str3 = ("karan thakurrr")
 my_str4 = my_str3.count("r")
 print(my_str4)
+
